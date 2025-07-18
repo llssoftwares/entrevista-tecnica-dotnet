3c5d07f3f89bb198f66b7101512358273815bbac
llssoftwares <lls.softwares@outlook.com>
2025-07-18 12:55:43 -0300

    Modificando algoritmo de recursão para versão iterativa, reduzindo a complexidade e aumentando a eficiência

--- src/BackendDesafio.API/Mappers/MenuMapper.cs
+++ src/BackendDesafio.API/Mappers/MenuMapper.cs
@@ -5,27 +5,23 @@ namespace BackendDesafio.API.Mappers;
 
 public static class MenuMapper
 {
-    public static List<MenuItemDto> GetMenu(List<MenuItem> menuItems) => BuildTree(null, menuItems);
-
-    // Constrói recursivamente uma estrutura de árvore a partir de uma lista plana de itens de menu.
-    private static List<MenuItemDto> BuildTree(int? parentId, List<MenuItem> menuItems)
+    public static List<MenuItemDto> GetMenu(List<MenuItem> menuItems)
     {
-        var result = new List<MenuItemDto>();
+        var dtoById = new Dictionary<int, MenuItemDto>();
 
-        foreach (var menuItem in menuItems)
+        foreach (var item in menuItems)
+            dtoById[item.Id] = new MenuItemDto(item.Id.ToString(), item.Name, []);
+
+        var roots = new List<MenuItemDto>();
+
+        foreach (var item in menuItems)
         {
-            if (menuItem.RelatedId != parentId) continue;
-
-            var tree = BuildTree(menuItem.Id, menuItems);
-
-            var dto = new MenuItemDto(
-                menuItem.Id.ToString(),
-                menuItem.Name,
-                tree);
-
-            result.Add(dto);
+            if (item.RelatedId == null)
+                roots.Add(dtoById[item.Id]);
+            else if (dtoById.TryGetValue(item.RelatedId.Value, out var parentDto))
+                parentDto.Submenus.Add(dtoById[item.Id]);
         }
 
-        return result;
+        return roots;
     }
 }
